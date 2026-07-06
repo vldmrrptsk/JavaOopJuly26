f13d047bece261c5745932a0e46b8c510b9c40d9
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2026-07-06 21:16:03 +0700

    Реализация класса Range, с дополнительными методами проверки попадания в диапозон и длиной диапазона.

--- Range/src/ru/academits/repetskiy/range/Range.java
+++ Range/src/ru/academits/repetskiy/range/Range.java
@@ -1,4 +1,35 @@
 package ru.academits.repetskiy.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getRange() {
+        return Math.abs(from - to);
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
 }
--- Range/src/ru/academits/repetskiy/range_main/Main.java
+++ Range/src/ru/academits/repetskiy/range_main/Main.java
@@ -1,7 +1,19 @@
 package ru.academits.repetskiy.range_main;
 
+import ru.academits.repetskiy.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello!");
+    public static void main(String[] args) {
+        Range range = new Range(-2.1, 10);
+
+        System.out.println("Длина диапазона: " + range.getRange());
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число для проверки попадания в диапазон: ");
+        double number = scanner.nextDouble();
+
+        System.out.println("Число " + number + " в диапазоне? " + range.isInside(number));
     }
 }
