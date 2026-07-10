abb3110aa4fe4185459887828e66e499d6d26565
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2026-07-10 22:35:48 +0700

    Реализация класса Range.

--- Range/src/ru/academits/repetskiy/range_main/Main.java
+++ Range/src/ru/academits/repetskiy/range_main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Range range = new Range(-2.1, 10);
 
-        System.out.println("Длина диапазона: " + range.getRange());
+        System.out.println("Длина диапазона: " + range.getInterval());
 
         Scanner scanner = new Scanner(System.in);
 
