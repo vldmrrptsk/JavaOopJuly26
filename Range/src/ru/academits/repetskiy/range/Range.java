f3c3bec889b4c884b183e9cd78e576899539198c
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2026-07-11 14:52:33 +0700

    Реализация класса Range.

--- Range/src/ru/academits/repetskiy/range/Range.java
+++ Range/src/ru/academits/repetskiy/range/Range.java
@@ -32,4 +32,47 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public boolean compareRangeBoundaries(Range range) {
+        return this.from > range.to || this.to < range.from;
+    }
+
+    public String[] getIntersection(Range range) {
+        if (compareRangeBoundaries(range)) {
+            return null;
+        }
+
+        double leftBoarderInterval = Math.max(this.from, range.from);
+        double rightBoarderInterval = Math.min(this.to, range.to);
+
+        return new String[]{leftBoarderInterval + "," + rightBoarderInterval};
+    }
+
+    public String[] getUnion(Range range) {
+        if (compareRangeBoundaries(range)) {
+            return new String[]{this.from + "," + this.to, range.from + "," + range.to};
+        }
+
+        double leftBoarderInterval = Math.min(this.from, range.from);
+        double rightBoarderInterval = Math.max(this.to, range.to);
+
+        return new String[]{leftBoarderInterval + "," + rightBoarderInterval};
+    }
+
+    public String[] getAsymmetricDifference(Range range) {
+        if (compareRangeBoundaries(range)) {
+            return null;
+        } else if (this.from < range.from) {
+            if (this.to < range.to) {
+                return new String[]{this.from + " " + range.from};
+            }
+
+            return new String[]{this.from + "," + range.from, range.to + "," + this.to};
+        } else if (this.from > range.from) {
+            if (this.to > range.to) {
+                return new String[]{range.from + " " + this.from};
+            }
+        }
+        return new String[]{range.from + "," + this.from, this.to + "," + range.to};
+    }
 }
--- Range/src/ru/academits/repetskiy/range_main/Main.java
+++ Range/src/ru/academits/repetskiy/range_main/Main.java
@@ -2,19 +2,38 @@ package ru.academits.repetskiy.range_main;
 
 import ru.academits.repetskiy.range.Range;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(-2.1, 10);
+        Range range1 = new Range(-2.1, 10);
 
-        System.out.println("Длина диапазона: " + range.getInterval());
+        System.out.println("Длина первого диапазона: " + range1.getInterval());
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите число для проверки попадания в диапазон: ");
+        System.out.print("Введите число для проверки попадания в первый диапазон: ");
         double number = scanner.nextDouble();
 
-        System.out.println("Число " + number + " в диапазоне? " + range.isInside(number));
+        System.out.println("Число " + number + " в первом диапазоне? " + range1.isInside(number));
+        System.out.println();
+
+        Range range2 = new Range(5, 20);
+        Range range3 = new Range(100, 101);
+        Range range4 = new Range(-10, -2.1);
+
+        Range[] ranges = new Range[]{range1, range2, range3, range4};
+
+        for (int i = 0; i < ranges.length - 1; i++) {
+            String intersection = Arrays.toString(ranges[i].getIntersection(ranges[i + 1]));
+            String union = Arrays.toString(ranges[i].getUnion(ranges[i + 1]));
+            String difference = Arrays.toString(ranges[i].getAsymmetricDifference(ranges[i + 1]));
+
+            System.out.printf("Интервал-пересечения %d и %d интервалов: %s%n", i + 1, i + 2, intersection);
+            System.out.printf("Интервал-объединения %d и %d интервалов: %s%n", i + 1, i + 2, union);
+            System.out.printf("Интервал-разности %d и %d интервалов: %s%n", i + 1, i + 2, difference);
+            System.out.println();
+        }
     }
 }
