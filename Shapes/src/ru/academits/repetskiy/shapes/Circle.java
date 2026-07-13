94ae4abed0c3acf11dd71bdd60d3be1607a4076c
VladimirRepetskiy <71264322+VladimirRepetskiy@users.noreply.github.com>
2026-07-13 21:23:11 +0700

    Реализация класса Shapes.

---
+++ Shapes/src/ru/academits/repetskiy/shapes/Circle.java
@@ -0,0 +1,58 @@
+package ru.academits.repetskiy.shapes;
+
+import java.util.Objects;
+
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double getWidth() {
+        return 2 * getRadius();
+    }
+
+    @Override
+    public double getHeight() {
+        return getWidth();
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(radius, circle.radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(radius);
+    }
+}
